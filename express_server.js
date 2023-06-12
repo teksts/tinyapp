c047af044f48ff32c93023965d163792d58f05d1
teksts <mark.egelhoff1@gmail.com>
2023-06-12 14:58:45 -0700

    Implemented a template for displaying urls and their shortened counterparts.

--- express_server.js
+++ express_server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
