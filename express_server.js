2607279707621c0265ffea4c1167477f8d8dc7ba
teksts <mark.egelhoff1@gmail.com>
2023-06-13 12:14:11 -0700

    Implemented a button for deleting shortened links from database

--- express_server.js
+++ express_server.js
@@ -46,6 +46,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
