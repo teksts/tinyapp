24bbcc7c0dcf32d40a019f818e4536dbe9c4bef4
teksts <mark.egelhoff1@gmail.com>
2023-06-13 12:34:56 -0700

    Implemented ability to edit longURLs for shortened link aliases already generated.

--- express_server.js
+++ express_server.js
@@ -46,6 +46,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newLongURL = req.body["longURL"];
+  urlDatabase[id] = newLongURL;
+  res.redirect('/urls');
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
